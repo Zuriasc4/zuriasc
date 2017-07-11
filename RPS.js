a1443fa548a319339c9641b5d451324b5a6cd22f
Zuriasc4 <zuricampbell@gmail.com>
2017-07-11 16:22:56 -0400

    first commit

---
+++ RPS.js
@@ -0,0 +1,14 @@
+var playerChoice = "r";
+
+var choices = ["r","p","s"]
+var compChoices[Math.floor(Math.random()*3)]
+
+if (playerChoice == compChoice){
+    console.log("It's a tie!");
+}
+
+if (playerChoice == "r" && compChoices == "p"){
+    console.log(Rock loses to paper. You lose!);{
+        
+    }
+}
