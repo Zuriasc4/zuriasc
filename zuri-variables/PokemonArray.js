3c13039c5ad7133f5b3186cea8a312e1b8cebdbe
Zuriasc4 <zuricampbell@gmail.com>
2017-07-11 13:05:13 -0400

    first commit

---
+++ zuri-variables/PokemonArray.js
@@ -0,0 +1,6 @@
+var myPokemonRoster = ["charizard", "wartortle", "meowth", "cubone", "ditto"];
+var firstToFight = myPokemonRoster[2];
+var secondToFIght = myPokemonRoster[4];
+
+for (var i=0; i<myPokemonRoster.length; i++){
+    console.log(myPokemonRoster[i])}
---
+++ zuri-variables/pokemonVariable.js
@@ -0,0 +1,7 @@
+var charizardHP = 78;
+var charizardNum = 6;
+var charizardType = "Fire and Flying"; 
+var charizardBestMove = "Flare blitz";
+var charizardLastEvo = true;
+
+console.log(charizardHP)
