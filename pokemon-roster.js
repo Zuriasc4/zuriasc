11493d89b2b13c07f39bbd12667affd569d20bdd
Zuriasc4 <zuricampbell@gmail.com>
2017-07-13 14:09:45 -0400

    worked on pokemon roster

--- pokemon-roster.js
+++ pokemon-roster.js
@@ -1,10 +1,19 @@
-var myPokemon = []
+var myPokemonRoster = []
 
-myPokemon[0]= ["Bulbasaur", "Grass", 45, 50, 50, "LEGEND"]
-myPokemon[1]= ["Charmander", "Fire", 45, 50, 50, "LEGEND"]
-myPokemon[2]= ["Squirtle", "Water", 45, 50, 50, "LEGEND"]
-myPokemon[3]= ["Dragonite", "Dragon", 91, DEF, ATK, "LEGEND"]
-myPokemon[4]= ["NAME", "TYPE", HP, DEF, ATK, "LEGEND"]
-myPokemon[5]= ["NAME", "TYPE", HP, DEF, ATK, "LEGEND"]
+// myPokemonRoster[0]= ["Bulbasaur", "Grass", 45, 50, 50, false]
+// myPokemonRoster[1]= ["Charmander", "Fire", 45, 50, 50, false]
+// myPokemonRoster[2]= ["Squirtle", "Water", 45, 50, 50, false]
+// myPokemonRoster[3]= ["Dragonite", "Dragon", 91, 95, 134, false]
+// myPokemonRoster[4]= ["Charizard", "Fire", 78, 78, 84, false]
+// myPokemonRoster[5]= ["Pikachu", "Electric", 35, 40, 55, false]
 
-console.log(myPokemon[][])
+// console.log("Pokemon: " + myPokemonRoster[0][0])
+
+
+myPokemonRoster= [["Bulbasaur", "Grass", 45, 50, 50, false], ["Charmander", "Fire", 45, 50, 50, false]
+
+var x = myPokemonRoster;
+
+for (i=0; i< 6; i ++){
+    console.log(myPokemonRoster[x])
+}
