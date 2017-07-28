f138f1d0734da7128991dce35a6b8fd691e81e40
Zuriasc4 <zuricampbell@gmail.com>
2017-07-28 15:00:39 -0400

    site

---
+++ zuri-ogrenow/ogre.js
@@ -0,0 +1,16 @@
+var playerHP = 100;
+var playerGold = 100;
+var playerDefeated = 0;
+// console.log("hello");
+function setup(){
+$("body").append("<h1>Hello</h1>");   
+$("body").append("HP:" + playerHP + ", Gold:" + playerGold + ", Defeated:" + playerDefeated);
+$("body").append("<img src="nyc.jpg">"
+$("body").append("img src= "nyc" + num + ".jpg"); 
+
+
+
+};
+
+    
+$(document).ready(setup);
